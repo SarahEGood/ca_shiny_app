3488c997a03d29fc221b3974498568c1e072f626
River <shoyren@gmail.com>
2022-06-04 23:10:52 +0100

    Datetime val converts to year only when displayed

--- app.R
+++ app.R
@@ -8,10 +8,12 @@ ui <- basicPage(
 
 server <- function(input, output) {
   df <- read.csv('ca_edu_data.csv')
+  df$Year <- as.POSIXct(df$Year, format= '%m/%d/%Y %H:%M:%S %p')
+  df$Year <- format(df$Year, format='%Y')
   
   output$plot1 <- renderPlot({
     ggplot(df, aes(Year, Population.Count, color=Personal.Income)) + geom_point()
-  })
+  }, res = 96)
   
   output$info <- renderText({
     paste0("x=", input$plot_click$x, "\ny=", input$plot_click$y)
